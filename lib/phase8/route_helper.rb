da6ea5f5a51916a58d97a2f09f8ba8386bd87fa0
Josh Trotter <jtrot1000@gmail.com>
2015-03-18 17:42:53 -0700

    cleanup RouteHelper

--- lib/phase8/route_helper.rb
+++ lib/phase8/route_helper.rb
@@ -8,15 +8,15 @@ module RouteHelper
   def button_to(name_string, path_parameters, method = :get)
     html = ""
     html += "<form action='#{parsed_path(path_parameters, method)}' "
-    html += "method='#{(method == :post) | (method == :delete) ? "post" : "get"}'>"
+    html += "method='#{(method == :post) | (method == :delete) ? 'post' : 'get'}'>"
     html += "\n<input type='submit' value='#{name_string}'>"
     html
   end
 
   def parsed_path(path_parameters, method)
-    prefixes = /^(.+)\_.+$/.match(path_parameters)[1].split("_")
+    prefixes = /^(.+)\_.+$/.match(path_parameters)[1].split('_')
     postfixes = /^.+\((.+)\)$/.match(path_parameters)
-    postfixes = postfixes[1].split(", ") unless postfixes.nil?
+    postfixes = postfixes[1].split(', ') unless postfixes.nil?
     p postfixes
     parsed_path = ""
 
@@ -27,9 +27,7 @@ module RouteHelper
       parsed_path += "#{postfixes[i]}/" unless postfixes[i].nil?
     end
 
-    if (method == :edit) | (method == :new)
-      parsed_path += "#{method.to_s}"
-    end
+    parsed_path += "#{method}" if (method == :edit) | (method == :new)
 
     parsed_path
   end
